38ad07196936ffa95687979dc635bd8225e601da
Marc MARTIN <marcmartin1@hotmail.com>
2021-04-22 22:47:58 +0200

    basic setup apollo/grapql to fetch data manually using client.query from local graphql server

--- src/index.js
+++ src/index.js
@@ -1,21 +1,52 @@
+import './index.css';
+
+import { ApolloClient, gql } from 'apollo-boost';
+import { persistor, store } from './redux/store';
+
+import { ApolloProvider } from 'react-apollo';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import { createHttpLink } from 'apollo-link-http';
 
-import { store, persistor } from './redux/store';
+const httpLink = createHttpLink({ uri: 'http://localhost:4000' });
 
-import './index.css';
-import App from './App';
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({ link: httpLink, cache });
+
+client
+	.query({
+		query: gql`
+			{
+				getCollectionsByTitle(title: "hats") {
+					id
+					title
+					items {
+						id
+						name
+						price
+						imageUrl
+					}
+				}
+			}
+		`,
+	})
+	.then((res) => console.log(res));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+	<ApolloProvider client={client}>
+		<Provider store={store}>
+			<BrowserRouter>
+				<PersistGate persistor={persistor}>
+					<App />
+				</PersistGate>
+			</BrowserRouter>
+		</Provider>
+	</ApolloProvider>,
+	document.getElementById('root')
 );
